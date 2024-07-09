372c1bfdbee33e07b8ce3fba45083fa7c5f05d70
Marek Mackovik <marek@mackovik.net>
2024-07-10 00:05:32 +0200

    Initial commit

---
+++ FormMain.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace TestExportUI
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnClose = new Button();
+            txtIV = new TextBox();
+            txtKEY = new TextBox();
+            txtDATA = new TextBox();
+            lblIV = new Label();
+            lblKEY = new Label();
+            lblDATA = new Label();
+            btnRun = new Button();
+            radioDecode = new RadioButton();
+            radioEncode = new RadioButton();
+            btnCopy = new Button();
+            txtOut2 = new TextBox();
+            txtOut1 = new TextBox();
+            SuspendLayout();
+            // 
+            // btnClose
+            // 
+            btnClose.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnClose.Location = new Point(1072, 464);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(75, 23);
+            btnClose.TabIndex = 0;
+            btnClose.Text = "&Zavřít";
+            btnClose.UseVisualStyleBackColor = true;
+            btnClose.Click += btnClose_Click;
+            // 
+            // txtIV
+            // 
+            txtIV.Location = new Point(17, 25);
+            txtIV.Name = "txtIV";
+            txtIV.Size = new Size(241, 23);
+            txtIV.TabIndex = 1;
+            txtIV.Text = "2436372c6566892db1ddbd65bc657082";
+            txtIV.TextChanged += txtIV_TextChanged;
+            // 
+            // txtKEY
+            // 
+            txtKEY.Location = new Point(279, 25);
+            txtKEY.Name = "txtKEY";
+            txtKEY.Size = new Size(241, 23);
+            txtKEY.TabIndex = 2;
+            txtKEY.Text = "aab8d355400b01e3dd521535acf0e110";
+            txtKEY.TextChanged += txtKEY_TextChanged;
+            // 
+            // txtDATA
+            // 
+            txtDATA.Location = new Point(541, 25);
+            txtDATA.Name = "txtDATA";
+            txtDATA.Size = new Size(241, 23);
+            txtDATA.TabIndex = 3;
+            txtDATA.Text = "2fcb3dd0eb2210a6896e1286ac673b0f";
+            txtDATA.TextChanged += txtDATA_TextChanged;
+            // 
+            // lblIV
+            // 
+            lblIV.AutoSize = true;
+            lblIV.Location = new Point(17, 7);
+            lblIV.Name = "lblIV";
+            lblIV.Size = new Size(17, 15);
+            lblIV.TabIndex = 5;
+            lblIV.Text = "IV";
+            // 
+            // lblKEY
+            // 
+            lblKEY.AutoSize = true;
+            lblKEY.Location = new Point(279, 7);
+            lblKEY.Name = "lblKEY";
+            lblKEY.Size = new Size(27, 15);
+            lblKEY.TabIndex = 6;
+            lblKEY.Text = "KEY";
+            // 
+            // lblDATA
+            // 
+            lblDATA.AutoSize = true;
+            lblDATA.Location = new Point(541, 7);
+            lblDATA.Name = "lblDATA";
+            lblDATA.Size = new Size(35, 15);
+            lblDATA.TabIndex = 7;
+            lblDATA.Text = "DATA";
+            // 
+            // btnRun
+            // 
+            btnRun.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnRun.Location = new Point(991, 464);
+            btnRun.Name = "btnRun";
+            btnRun.Size = new Size(75, 23);
+            btnRun.TabIndex = 8;
+            btnRun.Text = "&Spustit";
+            btnRun.UseVisualStyleBackColor = true;
+            btnRun.Click += btnRun_Click;
+            // 
+            // radioDecode
+            // 
+            radioDecode.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            radioDecode.AutoSize = true;
+            radioDecode.Checked = true;
+            radioDecode.Location = new Point(28, 468);
+            radioDecode.Name = "radioDecode";
+            radioDecode.Size = new Size(65, 19);
+            radioDecode.TabIndex = 9;
+            radioDecode.TabStop = true;
+            radioDecode.Text = "Decode";
+            radioDecode.UseVisualStyleBackColor = true;
+            radioDecode.CheckedChanged += radioDecode_CheckedChanged;
+            // 
+            // radioEncode
+            // 
+            radioEncode.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            radioEncode.AutoSize = true;
+            radioEncode.Location = new Point(115, 468);
+            radioEncode.Name = "radioEncode";
+            radioEncode.Size = new Size(64, 19);
+            radioEncode.TabIndex = 10;
+            radioEncode.Text = "Encode";
+            radioEncode.UseVisualStyleBackColor = true;
+            radioEncode.CheckedChanged += radioEncode_CheckedChanged;
+            // 
+            // btnCopy
+            // 
+            btnCopy.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnCopy.Location = new Point(831, 464);
+            btnCopy.Name = "btnCopy";
+            btnCopy.Size = new Size(129, 23);
+            btnCopy.TabIndex = 12;
+            btnCopy.Text = "&Do clipboardu";
+            btnCopy.UseVisualStyleBackColor = true;
+            btnCopy.Click += btnCopy_Click;
+            // 
+            // txtOut2
+            // 
+            txtOut2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            txtOut2.Font = new Font("Cascadia Mono SemiBold", 9F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            txtOut2.Location = new Point(17, 65);
+            txtOut2.Multiline = true;
+            txtOut2.Name = "txtOut2";
+            txtOut2.ScrollBars = ScrollBars.Both;
+            txtOut2.Size = new Size(562, 376);
+            txtOut2.TabIndex = 13;
+            // 
+            // txtOut1
+            // 
+            txtOut1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            txtOut1.Font = new Font("Cascadia Mono SemiBold", 9F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            txtOut1.Location = new Point(585, 65);
+            txtOut1.Multiline = true;
+            txtOut1.Name = "txtOut1";
+            txtOut1.ScrollBars = ScrollBars.Both;
+            txtOut1.Size = new Size(562, 376);
+            txtOut1.TabIndex = 14;
+            // 
+            // frmMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1165, 499);
+            Controls.Add(txtOut1);
+            Controls.Add(txtOut2);
+            Controls.Add(btnCopy);
+            Controls.Add(radioEncode);
+            Controls.Add(radioDecode);
+            Controls.Add(btnRun);
+            Controls.Add(lblDATA);
+            Controls.Add(lblKEY);
+            Controls.Add(lblIV);
+            Controls.Add(txtDATA);
+            Controls.Add(txtKEY);
+            Controls.Add(txtIV);
+            Controls.Add(btnClose);
+            Name = "frmMain";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "CBC Test";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Button btnClose;
+        private TextBox txtIV;
+        private TextBox txtKEY;
+        private TextBox txtDATA;
+        private Label lblIV;
+        private Label lblKEY;
+        private Label lblDATA;
+        private Button btnRun;
+        private RadioButton radioDecode;
+        private RadioButton radioEncode;
+        private Button btnCopy;
+        private TextBox txtOut2;
+        private TextBox txtOut1;
+    }
+}
---
+++ FormMain.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Diagnostics;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace TestExportUI
+{
+    public partial class frmMain : Form
+    {
+        private string strAkce = string.Empty;
+        private string strIV = string.Empty;
+        private string strKey = string.Empty;
+        private string strData = string.Empty;
+
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            strAkce = "-d";
+            strIV = txtIV.Text;
+            strKey = txtKEY.Text;
+            strData = txtDATA.Text;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void radioDecode_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioDecode.Checked)
+            {
+                radioEncode.Checked = false;
+                strAkce = "-d";
+            }
+        }
+
+        private void radioEncode_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioEncode.Checked)
+            {
+                radioDecode.Checked = false;
+                strAkce = "-e";
+            }
+        }
+
+        private void txtIV_TextChanged(object sender, EventArgs e)
+        {
+            strIV = txtIV.Text;
+        }
+
+        private void txtKEY_TextChanged(object sender, EventArgs e)
+        {
+            strKey = txtKEY.Text;
+        }
+
+        private void txtDATA_TextChanged(object sender, EventArgs e)
+        {
+            strData = txtDATA.Text;
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            string? exePath = Path.GetFullPath(@"d:\develop\TestExport\x64\Debug\TestExport.exe");
+            string exeParams = String.Concat(strAkce, " ", strIV, " ", strKey, " ", strData);
+
+            ProcessStartInfo startInfo = new ProcessStartInfo(exePath);
+            startInfo.Arguments = exeParams;
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardOutput = true;
+
+            Process? exeProcess = Process.Start(startInfo);
+
+            if (exeProcess != null)
+            {
+                // Synchronously read the standard output of the spawned process.
+                StreamReader reader = exeProcess.StandardOutput;
+                string output = reader.ReadToEnd();
+
+                if (exeProcess.WaitForExit(10000))
+                {
+                    txtOut2.Text = txtOut1.Text;
+                    txtOut1.Text = output;
+                }
+            }
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txtOut1.Text);
+        }
+    }
+}
